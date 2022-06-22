3d613a9d9ce96cbd8dad8d44c9bc1fb173e7cf84
jeffersonbecerra335@gmail.com <jeffersonbecerra335@gmail.com>
2022-06-21 21:23:03 -0500

    primer commit

---
+++ models/server.js
@@ -0,0 +1,51 @@
+import express from "express"
+import cors from "cors"
+import {dbConnection} from "../database/config.js"
+import persona from "../routes/persona.js"
+import comentario from "../routes/comentario.js"
+import pelicula from "../routes/pelicula.js"
+import actores from "../routes/actores.js"
+import favoritos from "../routes/favoritos.js"
+import fileUpload from "express-fileUpload"
+
+class Server{
+    constructor(){
+        this.app=express()
+        this.middleware()
+        this.port=process.env.PORT
+        this.conectarBd()
+        this.routes()
+    }
+
+    routes(){
+        this.app.use("/api/persona",persona)
+        this.app.use("/api/comentario",comentario)
+        this.app.use("/api/pelicula",pelicula)
+        this.app.use("/api/actores",actores)
+        this.app.use("/api/favoritos",favoritos)
+        
+    }
+
+    middleware(){
+        this.app.use(express.json())
+        this.app.use(cors())
+        this.app.use(fileUpload({
+            useTempFiles:true,
+            tempFileDir:'/tmp/',
+            createParentPath:true
+        }));
+    }
+    
+    async conectarBd(){
+        await dbConnection()
+    }
+
+    escuchar(){
+        this.app.listen(this.port,()=>{
+            console.log(`servidor escuchando en el puerto ${this.port}`);
+        })
+    }
+}
+
+export {Server}
+
---
+++ routes/actores.js
@@ -0,0 +1,18 @@
+import {Router} from "express";
+import {actorespost,actoresget,actoresGetId} from "../controllers/actores.js"
+import { validarCampos } from "../middlewares/middleware.js";
+import { check } from "express-validator"
+const router= new Router()
+
+router.post('/',[
+    check('nombre','el campo nombre no puede estar vacio').not().isEmpty(),
+    check('biografia','el campo biografia debe ser mayor a 6').isLength({min:6}),
+    validarCampos
+]
+,actorespost)
+
+router.get('/get',actoresget)
+
+router.get('/ID',actoresGetId)
+
+export default router
